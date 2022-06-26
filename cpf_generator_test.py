5a221fb6da874963d4bc0e7756203478a93f3334
HenriqueGibi <henriquedepadua@yahoo.com.br>
2022-06-26 19:44:49 -0300

    validador criado, falta o gerador e os testes

---
+++ cpf_generator_test.py
@@ -0,0 +1,9 @@
+from cpf_generator import validador_cpf
+from cpf_generator import gerador_cpf
+
+import unittest
+
+class MyTest(unittest.TestCase):
+
+    def test_para_ver_se_ta_tudo_certo(self):
+        self.assertEqual()
